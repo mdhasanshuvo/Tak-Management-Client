9d6160b546e9862810d9cd1e6b8edd153b0017e1
mdhasanshuvo <mdhasanshuvo220@gmail.com>
2025-02-21 01:32:32 +0600

    Authprovider addded

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,92 @@
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import { app } from "../firebase/firebase.config";
+
+export const AuthContext = createContext();
+
+
+const AuthProvider = ({ children }) => {
+
+    const provider = new GoogleAuthProvider();
+
+    const auth = getAuth(app);
+
+    const [user, setUser] = useState(null);
+
+    const [loading, setLoading] = useState(true);
+
+    const [email, setEmail] = useState('')
+
+    console.log(user);
+
+
+    const signUp = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const logout = () => {
+        setLoading(true);
+        signOut(auth);
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const googleAuth = () => {
+        return signInWithPopup(auth, provider);
+    }
+
+    const updateUser = (updatedData) => {
+        return updateProfile(auth.currentUser, updatedData);
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+
+
+        })
+
+        return () => {
+            unSubscribe();
+        }
+
+    }, []);
+
+    const [theme, setTheme] = React.useState('light');
+    const toggleTheme = () => {
+        setTheme(theme === 'dark' ? 'light' : 'dark');
+    };
+    // initially set the theme and "listen" for changes to apply them to the HTML tag
+    React.useEffect(() => {
+        document.querySelector('html').setAttribute('data-theme', theme);
+    }, [theme]);
+
+
+
+    const authValues = {
+        user,
+        setUser,
+        signUp,
+        logout,
+        signIn,
+        googleAuth,
+        loading,
+        updateUser,
+        email,
+        setEmail,
+        theme,
+        toggleTheme
+    }
+
+    return (
+        <AuthContext.Provider value={authValues}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider; 
