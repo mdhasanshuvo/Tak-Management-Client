adfe266b9c14b8f14ab2f73eef3d40d28197664d
mdhasanshuvo <mdhasanshuvo220@gmail.com>
2025-02-20 11:39:23 +0600

    Register page added

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 const AppRoutes = createBrowserRouter([
     {
@@ -9,6 +10,10 @@ const AppRoutes = createBrowserRouter([
     {
         path: '/auth/login',
         element: <Login></Login>
+    },
+    {
+        path: '/auth/register',
+        element: <Register></Register>
     },
 ]);
 
