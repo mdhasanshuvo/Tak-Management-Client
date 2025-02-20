6ebfddbe413ece714a7ccd7f2732b23f660c7db9
mdhasanshuvo <mdhasanshuvo220@gmail.com>
2025-02-20 11:24:27 +0600

    React Router Setup

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const AppRoutes = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello world!</div>,
+    },
+]);
+
+
+export default AppRoutes;
