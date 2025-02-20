03caf6666deb90b178bb22492e6ff2149cc4848a
mdhasanshuvo <mdhasanshuvo220@gmail.com>
2025-02-20 11:36:30 +0600

    Login page added

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Login from '../pages/Login';
 
 const AppRoutes = createBrowserRouter([
     {
         path: "/",
         element: <div>Hello world!</div>,
+    },
+    {
+        path: '/auth/login',
+        element: <Login></Login>
     },
 ]);
 
